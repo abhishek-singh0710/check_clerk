b1aa638102ffe2dc4967ca7a76ba9a31dab7bf03
abhishek-singh0710 <126397507+abhishek-singh0710@users.noreply.github.com>
2024-10-14 02:16:31 +0530

    Changed Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,33 @@
-// import { clerkMiddleware } from '@clerk/nextjs/server'
+// // import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// export default clerkMiddleware();
+// // export default clerkMiddleware();
+
+// // export const config = {
+// //   matcher: [
+// //     // Skip Next.js internals and all static files, unless found in search params
+// //     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+// //     // Always run for API routes
+// //     '/(api|trpc)(.*)',
+// //   ],
+// // }
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// // Define your public routes here
+// const isPublicRoute = createRouteMatcher([
+//   '/', 
+//   '/events/:id', 
+//   '/api/webhook/clerk', 
+//   '/api/webhook/stripe', 
+//   '/api/uploadthing'
+// ]);
+
+// export default clerkMiddleware((auth, request) => {
+//   // Protect all routes that are not public
+//   if (!isPublicRoute(request)) {
+//     auth().protect();
+//   }
+// });
 
 // export const config = {
 //   matcher: [
@@ -9,31 +36,27 @@
 //     // Always run for API routes
 //     '/(api|trpc)(.*)',
 //   ],
-// }
+// };
 
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// Define your public routes here
-const isPublicRoute = createRouteMatcher([
-  '/', 
-  '/events/:id', 
-  '/api/webhook/clerk', 
-  '/api/webhook/stripe', 
-  '/api/uploadthing'
-]);
-
-export default clerkMiddleware((auth, request) => {
-  // Protect all routes that are not public
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
   ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
